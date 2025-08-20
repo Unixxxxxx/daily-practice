77cfc4f5a1d1c758ed440a8470282a7f52eb9a92
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-20 16:28:20 +0530

    add template

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template, request , redirect 
 
 app = Flask(__name__)
 
 @app.route('/')
-
+def index():
+    return render_template('index.html', name='index')
 def home():
     return "hello"
+
 
 if __name__=='__main__':
     app.run(debug=True)
