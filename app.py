e014df5d9d6adfecb4d544221c719110cf1c8436
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-21 14:13:12 +0530

    modified

--- app.py
+++ app.py
@@ -9,8 +9,11 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html', name= 'home')
+@app.route('/about')
+def about():
+    return render_templates('about.html', name= 'about')
 @app.route('/template')
-def templates():
+def template():
     return render_template('templates.html', name='templates')
 
 if __name__=='__main__':
