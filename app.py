60e0062392ac73e6f6e5345412357e1fc24bc00c
Unixxxxxx <sudhanshuroyss208@gmail.com>
2025-08-23 16:47:18 +0530

    modified and add file

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def about():
 @app.route('/template')
 def template():
     return render_template('templates.html', name='templates')
+@app.route('/', methods=['GET','POST'])
+def form():
+    if request.method == 'POST':
+        name= required.form.get("name")
+        email = request.form.get("email")
+        return f"Form submitred! Name: {name}, Email:{email}"
+    return render_template("form.hml")
+    
 
 if __name__=='__main__':
     app.run(debug=True)
